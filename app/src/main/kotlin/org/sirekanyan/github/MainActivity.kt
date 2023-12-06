92d78432aa28d0462a5c9283c01f9d8e90a3678d
Vadik Sirekanyan <me@sirekanyan.org>
2023-12-06 20:34:39 +0400

    Added back press callback

--- app/src/main/kotlin/org/sirekanyan/github/MainActivity.kt
+++ app/src/main/kotlin/org/sirekanyan/github/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import org.sirekanyan.github.data.model.GithubRepo
 import org.sirekanyan.github.databinding.GhMainActivityBinding
@@ -18,22 +19,30 @@ import org.sirekanyan.github.utils.showToast
 class MainActivity : AppCompatActivity(), RepoListPresenter.Router, RepoDetailsPresenter.Router {
 
     private val binding by lazy { GhMainActivityBinding.inflate(layoutInflater) }
-
     private val listPresenter: RepoListPresenter by lazy {
         RepoListPresenterImpl(this, app().githubApi).also {
             it.view = RepoListViewImpl(binding.repoListView, it)
         }
     }
-
     private val detailsPresenter: RepoDetailsPresenter by lazy {
         RepoDetailsPresenterImpl(this).also {
             it.view = RepoDetailsViewImpl(binding.repoDetailsView, it)
+        }
+    }
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (detailsPresenter.isShown) {
+                detailsPresenter.show(repo = null)
+            } else {
+                finish()
+            }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        onBackPressedDispatcher.addCallback(onBackPressedCallback)
     }
 
     override fun onStart() {
